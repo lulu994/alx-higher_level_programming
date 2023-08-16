2ffcfcc66b5375dfd452f446796057f565f201d8
lulu994 <loubna.neddah@gmail.com>
2023-08-16 13:52:35 +0200

    adding files

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,11 +1,5 @@
--- Create user 'user_0d_1' with password 'user_0d_1_pwd'
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-
 -- Grant all privileges to 'user_0d_1'
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-
--- Create user 'user_0d_2' with password 'user_0d_2_pwd'
-CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION
 
 -- Grant all privileges to 'user_0d_2'
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost' WITH GRANT OPTION;
