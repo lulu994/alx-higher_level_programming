91b8c46be9499300123e2af89e85e7527625ba3f
lulu994 <loubna.neddah@gmail.com>
2023-09-29 12:10:58 +0200

    What is my status?

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status"""
-import urllib.request
-
-
-def fetcher():
-    """fetcher"""
-    with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
-        html = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode("utf-8")))
+"""
+Python script that fetches https://intranet.hbtn.io/status
+"""
+from urllib import request
 
 if __name__ == "__main__":
-    fetcher()
+
+    req = request.Request("https://intranet.hbtn.io/status")
+
+    with request.urlopen(req) as response:
+        body = response.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode("utf-8")))
