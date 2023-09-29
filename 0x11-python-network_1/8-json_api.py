7bca103c75e921a8e9c578379ab7c01bad64e27f
lulu994 <loubna.neddah@gmail.com>
2023-09-29 13:09:02 +0200

    Search API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python3
-"""
-Script that takes in a letter and sends a POST request to
-http://0.0.0.:5000/search_user with the letter as a  parameter
-"""
+"""Check status"""
 import requests
-from sys import argv
+import sys
 
-if __name__ == "__main__":
-    url = "http://0.0.0.0:5000/search_user"
-    if len(argv) == 1:
-        print("No result")
-        quit()
-    letter = argv[1]
-    res = requests.post(url, data={"q": letter})
+
+def searchapi():
+    """status"""
+    if len(sys.argv) == 1:
+        q = ""
+    else:
+        q = sys.argv[1]
+
+    result = requests.post("http://0.0.0.0:5000/search_user", data={"q": q})
+
     try:
-        res_json = res.json()
+        data = result.json()
+        if data:
+            print("[{}] {}".format(data["id"], data["name"]))
+        else:
+            print("No result")
     except:
         print("Not a valid JSON")
-    else:
-        if len(res_json) == 0:
-            print("No result")
-        else:
-            print("[{}] {}".format(res_json["id"], res_json["name"]))
+
+if __name__ == "__main__":
+    searchapi()
