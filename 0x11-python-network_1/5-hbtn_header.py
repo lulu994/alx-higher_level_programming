d71afcda69af57efc191e486f571f5703ad2c3f1
lulu994 <loubna.neddah@gmail.com>
2023-09-29 12:46:52 +0200

    Response header value #1

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
-"""Check status"""
+"""
+Script that takes in a URL, sends a request to the URL and displays the value
+of the variable X-Request-Id in the response header
+"""
 import requests
-import sys
-
-
-def header():
-    """status"""
-    result = requests.get(sys.argv[1])
-
-    print(result.headers.get("X-Request-Id", None))
+from sys import argv
 
 if __name__ == "__main__":
-    header()
+    res = requests.get(argv[1])
+    print(res.headers.get("X-Request-Id"))
