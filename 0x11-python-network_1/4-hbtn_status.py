cee67f98e140f2365b6db7078616f843520fd57c
lulu994 <loubna.neddah@gmail.com>
2023-09-29 12:37:48 +0200

    What is my status #1

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Script that fetches https://intranet.hbtn.io/status
+"""
+import requests
+
+if __name__ == "__main__":
+    res = requests.get("https://intranet.hbtn.io/status")
+    body = res.text
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
