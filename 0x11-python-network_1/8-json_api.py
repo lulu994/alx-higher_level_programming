b821a2b2bb7810fa40bb8bb9dfe08d7a7029c5f8
lulu994 <loubna.neddah@gmail.com>
2023-09-29 13:06:56 +0200

    Search API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -4,32 +4,21 @@ Script that takes in a letter and sends a POST request to
 http://0.0.0.:5000/search_user with the letter as a  parameter
 """
 import requests
-import sys
-
-def search_user(letter):
-    url = 'http://0.0.0.0:5000/search_user'
-    payload = {'q': letter}
-
-    try:
-        response = requests.post(url, data=payload)
-        response.raise_for_status()  # Raise an exception for HTTP errors
-
-        data = response.json()
-        if data:
-            print(f"[{data['id']}] {data['name']}")
-        else:
-            print("No result")
-    except requests.exceptions.HTTPError as e:
-        print("No result")
-    except ValueError:
-        print("Not a valid JSON")
+from sys import argv
 
 if __name__ == "__main__":
-    if len(sys.argv) == 1:
-        letter = ""
-    elif len(sys.argv) == 2:
-        letter = sys.argv[1]
+    url = "http://0.0.0.0:5000/search_user"
+    if len(argv) == 1:
+        print("No result")
+        quit()
+    letter = argv[1]
+    res = requests.post(url, data={"q": letter})
+    try:
+        res_json = res.json()
+    except:
+        print("Not a valid JSON")
     else:
-        sys.exit("Usage: ./8-json_api.py [letter]")
-
-    search_user(letter)
+        if len(res_json) == 0:
+            print("No result")
+        else:
+            print("[{}] {}".format(res_json["id"], res_json["name"]))
